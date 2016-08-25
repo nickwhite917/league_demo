00b115fcee38e10dc3d3252d129293f06856353c
Nicholas White <nick@nickwhite.us>
2016-08-24 22:00:15 -0400

    Fix ENV

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-if(!process.ENV.heroku){
+if(!process.env.heroku){
   dotenv.load({ path: '.env' });
 }
 
