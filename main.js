672affccaeb85cd48745652b306295b01a504008
nick <nickwhite917@yahoo.com>
2016-08-26 08:11:05 -0400

    Move PM2 to Main.js from App.js to prevent recursive calling.

---
+++ main.js
@@ -0,0 +1,35 @@
+var pm2 = require('pm2');
+
+var MACHINE_NAME = 'hk1';
+var PRIVATE_KEY  = process.env.KEYMETRICS_PRIVATE_KEY || 'hz16fkkh5yuiesc' ;   // Keymetrics Private key
+var PUBLIC_KEY   = process.env.KEYMETRICS_PUBLIC_KEY || 'ka8euivsest49or';   // Keymetrics Public  key
+
+var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
+var maxMemory = process.env.WEB_MEMORY      || 512;// " " "
+
+//PM2
+pm2.connect(function() {
+  pm2.start({
+    script    : 'app.js',
+    exec_mode : 'cluster',            // ----> https://github.com/Unitech/PM2/blob/master/ADVANCED_README.md#schema
+    instances : instances,
+    max_memory_restart : maxMemory + 'M',   // Auto restart if process taking more than XXmo
+    post_update: ["npm install"]       // Commands to execute once we do a pull from Keymetrics
+  }, function() {
+    pm2.interact(PRIVATE_KEY, PUBLIC_KEY, MACHINE_NAME, function() {
+    
+     // Display logs in standard output 
+     pm2.launchBus(function(err, bus) {
+       console.log('[PM2] Log streaming started');
+ 
+       bus.on('log:out', function(packet) {
+        console.log('[App:%s] %s', packet.process.name, packet.data);
+       });
+        
+       bus.on('log:err', function(packet) {
+         console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+       });
+      });
+    });
+  });
+});
